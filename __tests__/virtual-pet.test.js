db558026397714825b193fc3becb2ce3d8cee50a
clayton-scherer <cscherer93@aol.com>
2019-05-17 11:29:47 -0400

    added entertainment to constructor and tests.

--- __tests__/virtual-pet.test.js
+++ __tests__/virtual-pet.test.js
@@ -3,15 +3,22 @@ const virtualPet = require("../src/virtual-pet.js");
 describe('virtual pet', () => {
     let underTest;
     test('Should return energy stat', () => {
-        const underTest = new virtualPet(100);
+        const underTest = new virtualPet(50, 50);
         const actual = underTest.energy;
-        expect(actual).toEqual(100); 
+        expect(actual).toEqual(50); 
     });
 
-    test('When pet is fed energy should increase', () =>{
-        const underTest = new virtualPet(10);
+    test('When pet is fed energy should increase', () => {
+        const underTest = new virtualPet(50, 50);
         underTest.eat();
         const actual = underTest.energy;
-        expect(actual).toEqual(20);
+        // Here our toEqual is 60 because we are calling the eat method.
+        expect(actual).toEqual(60);
+    });
+
+    test('When pet is played with entertainment should increase', () => {
+        const underTest = new virtualPet(50, 50);
+        const actual = underTest.entertainment;
+        expect(actual).toEqual(50);
     });
 });
--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -5,15 +5,18 @@
     this._z = z;
 };*/
 class VirtualPet {
-    constructor(energy) {
+    constructor(energy, entertainment) {
         this._energy = energy;
-    }
-    get energy () {
+        this._entertainment = entertainment;
+    };
+    get energy() {
         return this._energy;
-    }
+    };
+    get entertainment() {
+        return this._entertainment;
+    };
     eat(){
         this._energy += 10;
-    }
-}
-
+    };
+};
 module.exports = VirtualPet;
