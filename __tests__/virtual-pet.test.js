ffbbdb6d29c33e1dcee0362011069d7b39da330f
clayton-scherer <cscherer93@aol.com>
2019-05-17 11:58:29 -0400

    added hygene stat and bathe method.

--- __tests__/virtual-pet.test.js
+++ __tests__/virtual-pet.test.js
@@ -1,24 +1,36 @@
 const virtualPet = require("../src/virtual-pet.js");
 
-describe('virtual pet', () => {
-    let underTest;
-    test('Should return energy stat', () => {
-        const underTest = new virtualPet(50, 50);
-        const actual = underTest.energy;
-        expect(actual).toEqual(50); 
-    });
+describe("virtual pet", () => {
+  let underTest;
+  test("Should return energy stat", () => {
+    const underTest = new virtualPet(50, 50, 50);
+    const actual = underTest.energy;
+    expect(actual).toEqual(50);
+  });
 
-    test('When pet is fed energy should increase', () => {
-        const underTest = new virtualPet(50, 50);
-        underTest.eat();
-        const actual = underTest.energy;
-        // Here our toEqual is 60 because we are calling the eat method.
-        expect(actual).toEqual(60);
-    });
+  test("When pet is fed energy should increase", () => {
+    const underTest = new virtualPet(50, 50, 50);
+    underTest.feed();
+    const actual = underTest.energy;
+    // Here our toEqual is 60 because we are calling the eat method.
+    expect(actual).toEqual(60);
+  });
 
-    test('When pet is played with entertainment should increase', () => {
-        const underTest = new virtualPet(50, 50);
-        const actual = underTest.entertainment;
-        expect(actual).toEqual(50);
-    });
+  test("Should return entertainment stat && When pet is played with entertainment should increase", () => {
+    const underTest = new virtualPet(50, 50, 50);
+    underTest.play();
+    const actual = underTest.entertainment;
+    expect(actual).toEqual(60);
+  });
+  test("Should return hygene stat", () => {
+    const underTest = new virtualPet(50, 50, 50);
+    const actual = underTest.hygene;
+    expect(actual).toEqual(50);
+  });
+  test("When pet is bathed, hygene stat should increase", () => {
+    const underTest = new virtualPet(50, 50, 50);
+    underTest.bathe();
+    const actual = underTest.hygene;
+    expect(actual).toEqual(60);
+  });
 });
--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -1,22 +1,31 @@
-
 /* constructor(x, y, z) {
     this._x = x;
     this._y = y;
     this._z = z;
 };*/
 class VirtualPet {
-    constructor(energy, entertainment) {
-        this._energy = energy;
-        this._entertainment = entertainment;
-    };
-    get energy() {
-        return this._energy;
-    };
-    get entertainment() {
-        return this._entertainment;
-    };
-    eat(){
-        this._energy += 10;
-    };
-};
+  constructor(energy, entertainment, hygene) {
+    this._energy = energy;
+    this._entertainment = entertainment;
+    this._hygene = hygene;
+  }
+  get energy() {
+    return this._energy;
+  }
+  get entertainment() {
+    return this._entertainment;
+  }
+  get hygene() {
+    return this._hygene;
+  }
+  feed() {
+    this._energy += 10;
+  }
+  play() {
+    this._entertainment += 10;
+  }
+  bathe() {
+    this._hygene += 10;
+  }
+}
 module.exports = VirtualPet;
