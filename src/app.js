b383a563ab9f5c052c256259862b1baabd6820ab
clayton-scherer <cscherer93@aol.com>
2019-05-17 13:46:36 -0400

    modified the app file to include installing chalk, welcome message and first interactive options; as well as 5 of 5 tests pass.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 // Dependencies
-const input = require('readline-sync');
-const virtualPet = require('./virtual-pet');
+const input = require("readline-sync");
+const virtualPet = require("./virtual-pet");
+const chalk = require("chalk");
+const blueText = chalk.blue;
 // New instance of virtualPet
-const newInstance = new virtualPet;
+const newInstance = new virtualPet(50, 50, 50);
 // \n is a linebreak command.
-console.log('Hello!\nWelcome to Virtual Pet!!!');
-const userResponse = input.question('Do you wana build a snowman?!\n');
-
+// Welcome message and first interactive options.
+console.log("Hello!\nWelcome to Virtual Pet!!!");
+const userResponse = input.question(
+  "How would you like to interact with your pet?\n" +
+    `Enter "1" for ${blueText("Feed")}\n` +
+    `Enter "2" for ${blueText("Play")}\n` +
+    `Enter "3" for ${blueText("Bathe")}\n` +
+    `Enter "4" for ${blueText("Quit")}\n`
+);
